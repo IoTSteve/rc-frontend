86c23c5ab1c77861e9163c690a46eae9666dffa3
Steve <S_Reich.1@web.de>
2020-08-01 19:20:47 +0200

    restyled frontend
    pulled new style submodule, styled wast buttons and added wasd

--- src/components/controls.vue
+++ src/components/controls.vue
@@ -1,39 +1,39 @@
 <template>
-  <div>
+  <div style="height:400px;">
     <div>Status: {{connectedMsg}}</div>
-    <b-button class="buttons"
+    <b-button class="buttons" id="button1"
       :disabled="!connected || !$store.getters.amIActive" v-on:click="go()">
       Forward
       <br>
       W / <b-icon icon="arrow-up-square"></b-icon>
     </b-button>
-    <b-button class="buttons"
+    <b-button class="buttons" id="button2"
       v-show="$store.getters.authorized"
       :disabled="!connected || !$store.getters.amIActive" v-on:click="back()">
       Back
       <br>
       S / <b-icon icon="arrow-down-square"></b-icon>
     </b-button>
-    <b-button class="buttons"
+    <b-button class="buttons" id="button3"
       :disabled="!connected || !$store.getters.amIActive" v-on:click="turnLeft()">
       Left
       <br>
       A / <b-icon icon="arrow-left-square"></b-icon>
     </b-button>
-    <b-button class="buttons"
+    <b-button class="buttons" id="button4"
       :disabled="!connected || !$store.getters.amIActive" v-on:click="turnRight()">
       Right
       <br>
       D / <b-icon icon="arrow-right-square"></b-icon>
     </b-button>
-    <b-button class="buttons"
+    <b-button class="buttons" id="button5"
       :disabled="!connected || !$store.getters.amIActive" v-on:click="stop()">
       Stop
       <br>
-      Space
+      S
     </b-button>
     <br>
-    <b-button class="buttons"
+    <b-button class="buttons" id="button6"
       v-show="$store.getters.authorized"
       :disabled="!connected || !$store.getters.amIActive" v-on:click="shutdown()">
       shutdown
@@ -93,9 +93,9 @@ export default {
         this.turnRight();
       } else if (e.code === 'ArrowDown') {
         this.back();
+        /* } else if (e.code === 'KeyS') {
+        this.back(); */
       } else if (e.code === 'KeyS') {
-        this.back();
-      } else if (e.code === 'Space') {
         this.stop();
       }
       // do stuff
@@ -143,5 +143,39 @@ export default {
 <style>
   .buttons {
     margin: 10px;
+    position:absolute;
+  }
+  #button1 {
+top: 60px;
+left: 210px;
+  }
+  #button2 {
+left: 40px;
+top:400px;
+  }
+  #button3 {
+left: 110px;
+top:200px;
+  }
+  #button4 {
+left: 350px;
+top:200px;
+  }
+  #button5 {
+left: 230px;
+top:200px;
+  }
+  #button6 {
+left: 40px;
+top:200px;
+  }
+
+  input{
+    position:absolute;
+    bottom: 0px;
+    left:0px;
+    right:0px;
+    margin-left:auto;
+    margin-right:auto;
   }
 </style>
